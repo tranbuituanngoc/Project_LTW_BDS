5dfe453f8e51d314dd72f295d00e48f83114ef9a
tranbuituanngoc <20130337@st.hcmuaf.edu.vn>
2022-11-03 21:09:07 +0700

    update nav

--- js/script.js
+++ js/script.js
@@ -15,3 +15,22 @@ function showComPass() {
         x.type = "password";
     }
 }
+
+function changeSliderValue(img, src) {
+    document.getElementById("mainPhoto")
+}
+var myIndex = 0;
+carousel();
+
+// function carousel() {
+//     var i;
+//     var x = document.getElementsByClassName("mySlider");
+//     for (i = 0; i < x.length; i++) {
+//         x[i].style.display = "none";
+//     }
+//     myIndex++;
+//     if (myIndex > x.length) { myIndex = 1 }
+//     x[myIndex - 1].style.display = "block";
+//     setTimeout(carousel, 2000);
+//     console.log(2)
+// }
