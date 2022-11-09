3eca9b38615f252698bffee863288d62e7beaf02
tranbuituanngoc <20130337@st.hcmuaf.edu.vn>
2022-11-09 22:46:46 +0700

    update Index, Slider Index,...

--- js/custom.js
+++ js/custom.js
@@ -148,8 +148,13 @@ jQuery(function($) {
         infinite: true,
         arrows: true,
         speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
         fade: true,
         cssEase: 'linear'
+
     });
 
     /* ----------------------------------------------------------- */
